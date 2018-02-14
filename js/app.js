e9f38bff9d2ee02dc396d2dce8ca11960763872e
milagrospalma <mnpalma.2@gmail.com>
2018-02-13 20:38:42 -0500

    Añade archivo js

---
+++ js/app.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', function() {
+  const form = document.getElementById('search-form');
+  const searchField = document.getElementById('search-keyword');
+  const responseContainer = document.getElementById('response-container');
+  let searchedForText;
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = '';
+    searchedForText = searchField.value;
+    getNews();
+  });
+
+  function getNews() {
+    const articleRequest = new XMLHttpRequest();
+    articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=c763804a85dd49e3a98e726fdbd114a2`);
+    articleRequest.onload = addNews;
+    articleRequest.onerror = handleError;
+    articleRequest.send();
+  }
+
+  function handleError() {
+    console.log('Se presentó un error.');
+  }
+
+  function addNews() {
+    const data = JSON.parse(this.responseText);
+    const article = data.response.docs[0];
+    const title = article.headline.main;
+    const snippet = article.snippet;
+
+    let li = document.createElement('li');
+    li.className = 'articleClass';
+    li.innerText = snippet;
+
+    responseContainer.appendChild(li);
+  }
+});
